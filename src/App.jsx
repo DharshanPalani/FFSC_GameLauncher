7b92e7b5f36bb52c11415055ff0a7976c6259735
Dhars <dharshanpalani.2006@gmail.com>
2025-04-25 22:20:35 +0530

    Batman

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="left">
+        <h1>Image testing</h1>
+      </div>
+      <div className="right">
+        <div className="title">
+          <h1>Friendly</h1>
+          <h1>Friends</h1>
+          <h1>Student Council</h1>
+        </div>
+
+        <div className="buttons">
+          <button>
+            <p>Start</p>
+          </button>
+          <button>
+            <p>Update</p>
+          </button>
+        </div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
